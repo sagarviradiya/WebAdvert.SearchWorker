24b1ed9f70c833b7c94b82b0b7ceb844bdbba383
Sagar <sviradiya@fintechglobal.center>
2020-06-28 16:06:05 +0530

    Add project files.

---
+++ SearchWorker.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.Json;
+using Amazon.Lambda.SNSEvents;
+
+[assembly: LambdaSerializer(typeof(JsonSerializer))]
+namespace WebAdvert.SearchWorker
+{
+    public class SearchWorker
+    {
+        public void Function(SNSEvent snsEvent,ILambdaContext context)
+        {
+            foreach (var item in snsEvent.Records)
+            {
+                context.Logger.LogLine(item.Sns.Message);
+            }
+        }
+    }
+}
